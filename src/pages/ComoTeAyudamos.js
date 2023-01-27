189f22651841169b7ed388d59b48fff1a90ae016
juanmoyano123 <moyanojjeronimo@gmail.com>
2023-01-27 10:28:46 -0300

    update

--- src/pages/ComoTeAyudamos.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function ComoTeAyudamos() {
-  return (
-    <div>ComoTeAyudamos</div>
-  )
-}
--- src/pages/UltimoProyectos.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function UltimoProyectos() {
-  return (
-    <div>UltimoProyectos</div>
-  )
-}
