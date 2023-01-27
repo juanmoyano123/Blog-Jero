cbfe52219a98d468095a200301423639082b1bf6
juanmoyano123 <moyanojjeronimo@gmail.com>
2023-01-27 11:21:14 -0300

    instalo tailwind

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import Encabezamiento from './componentes/compartidos/Encabezamiento';
+import Principal from './componentes/compartidos/Principal';
+import Pie from "./componentes/compartidos/Pie"
+
 
 function App() {
   return (
     <div className="App">
-     <h1>To do List</h1>
-      
-      
+     <h1 >To do List</h1>
+      <Encabezamiento></Encabezamiento>
+      <Principal></Principal>
+      <Pie></Pie>
     </div>
   );
 }
