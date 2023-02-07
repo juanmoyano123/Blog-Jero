cdec52b929ef1af32ac53c3ec36c21a54989d46c
juanmoyano123 <moyanojjeronimo@gmail.com>
2023-02-07 10:15:43 -0300

    instalacion de useContext + prueba

--- src/componentes/lista/Lista.jsx
+++ src/componentes/lista/Lista.jsx
@@ -1,39 +1,47 @@
 import Meta from "./Meta";
+import { useContext } from "react";
+import { Contexto } from "../../servicios/Memoria";
+
+const lista = [
+  {
+    id: 1,
+    detalles: "Correr por 30 minutos",
+    periodo: "dia",
+    eventos: 1,
+    icono: "🏃‍♂️",
+    meta: 366,
+    plazo: "2030/01/01",
+    completado: 100,
+  },
+  {
+    id: 1,
+    detalles: "Correr por 30 minutos",
+    periodo: "dia",
+    eventos: 1,
+    icono: "🏃‍♂️",
+    meta: 366,
+    plazo: "2030/01/01",
+    completado: 100,
+  },
+  {
+    id: 1,
+    detalles: "Leer libros",
+    periodo: "dia",
+    eventos: 1,
+    icono: "🏃‍♂️",
+    meta: 366,
+    plazo: "2030/01/01",
+    completado: 100,
+  },
+];
 
 function Lista() {
-  const lista = [
-    {
-      id: 1,
-      detalles: "Correr por 30 minutos",
-      periodo: "dia",
-      eventos: 1,
-      icono: "🏃‍♂️",
-      meta: 366,
-      plazo: "2030/01/01",
-      completado: 100,
-    },
-    {
-      id: 1,
-      detalles: "Correr por 30 minutos",
-      periodo: "dia",
-      eventos: 1,
-      icono: "🏃‍♂️",
-      meta: 366,
-      plazo: "2030/01/01",
-      completado: 100,
-    },
-    {
-      id: 1,
-      detalles: "Leer libros",
-      periodo: "dia",
-      eventos: 1,
-      icono: "🏃‍♂️",
-      meta: 366,
-      plazo: "2030/01/01",
-      completado: 100,
-    },
-  ];
-  return lista.map((meta) => <Meta {...meta}></Meta>);
+  const prueba = useContext(Contexto);
+  console.log(prueba);
+
+  return (
+    lista.map((meta) => <Meta {...meta}></Meta>)
+    );
 }
 
 export default Lista;
---
+++ src/servicios/Memoria.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { createContext } from "react";
+
+export const Contexto = createContext(null);
+
+function Memoria({ children }) {
+  return (
+  <Contexto.Provider value={"compartido"}>
+    {children}
+    </Contexto.Provider>
+    );
+}
+
+export default Memoria;
